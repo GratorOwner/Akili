732b9c36dbeb7d1cd892034968ece37301dd8e55
GratorOwner <153442558+GratorOwner@users.noreply.github.com>
2026-02-05 03:31:02 -0500

    Update search.js

--- McpServer/src/tools/search.js
+++ McpServer/src/tools/search.js
@@ -1,1 +1,17 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const API_URL = process.env.AKILI_API_URL;
+const API_KEY = process.env.AKILI_API_KEY;
+
+export async function searchBlackOwned(location, radius_km) {
+  const response = await axios.get(`${API_URL}/search`, {
+    params: { location, radius_km },
+    headers: { "x-api-key": API_KEY }
+  });
+
+  return response.data;
+}
 
